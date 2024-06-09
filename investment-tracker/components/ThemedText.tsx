780d76ce2a5d7711e01e277149420cb7113d04ec
Marcus Peh <hkmpeh@gmail.com>
2024-06-09 22:29:27 +0800

    Update font scheme

--- investment-tracker/components/ThemedText.tsx
+++ investment-tracker/components/ThemedText.tsx
@@ -5,7 +5,7 @@ import { useThemeColor } from '@/hooks/useThemeColor';
 export type ThemedTextProps = TextProps & {
   lightColor?: string;
   darkColor?: string;
-  type?: 'default' | 'title' | 'defaultSemiBold' | 'subtitle' | 'link';
+  type?: 'default' | 'h4' | 'h5'  | 's1' | 's2' | 'l1' | 'l2' | 'l3' | 'p1' | 'p2' | 'b1' | 'b2';
 };
 
 export function ThemedText({
@@ -22,10 +22,17 @@ export function ThemedText({
       style={[
         { color },
         type === 'default' ? styles.default : undefined,
-        type === 'title' ? styles.title : undefined,
-        type === 'defaultSemiBold' ? styles.defaultSemiBold : undefined,
-        type === 'subtitle' ? styles.subtitle : undefined,
-        type === 'link' ? styles.link : undefined,
+        type === 'h4' ? styles.h4 : undefined,
+        type === 'h5' ? styles.h5 : undefined,
+        type === 's1' ? styles.s1 : undefined,
+        type === 's2' ? styles.s2 : undefined,
+        type === 'l1' ? styles.l1 : undefined,
+        type === 'l2' ? styles.l2 : undefined,
+        type === 'l3' ? styles.l3 : undefined,
+        type === 'p1' ? styles.p1 : undefined,
+        type === 'p2' ? styles.p2 : undefined,
+        type === 'b1' ? styles.b1 : undefined,
+        type === 'b2' ? styles.b2 : undefined,
         style,
       ]}
       {...rest}
@@ -38,23 +45,55 @@ const styles = StyleSheet.create({
     fontSize: 16,
     lineHeight: 24,
   },
-  defaultSemiBold: {
-    fontSize: 16,
-    lineHeight: 24,
-    fontWeight: '600',
-  },
-  title: {
+  h4: {
     fontSize: 32,
-    fontWeight: 'bold',
-    lineHeight: 32,
+    fontWeight: '800',
+    lineHeight: 33,
+    letterSpacing: 0.25,
   },
-  subtitle: {
-    fontSize: 20,
-    fontWeight: 'bold',
+  h5: {
+    fontSize: 32,
+    fontWeight: '600',
+    lineHeight: 23,
   },
-  link: {
-    lineHeight: 30,
+  s1: {
+    fontSize: 15,
+    letterSpacing: 0.15,
+  },
+  s2: {
+    fontSize: 13,
+    letterSpacing: 0.1,
+  },
+  l1: {
+    fontSize: 15,
+    fontWeight: 'semibold',
+  },
+  l2: {
+    fontSize: 10,
+    lineHeight: 16,
+    fontWeight: 'semibold',
+  },
+  l3: {
+    fontSize: 10,
+    lineHeight: 16,
+    fontWeight: 'semibold',
+  },
+  p1: {
+    fontSize: 15,
+    letterSpacing: 0.5,
+  },
+  p2: {
+    fontSize: 13,
+    letterSpacing: 0.25,
+  },
+  b1: {
     fontSize: 16,
-    color: '#0a7ea4',
+    fontWeight: 'bold',
+    lineHeight: 24,
   },
+  b2: {
+    fontSize: 14,
+    fontWeight: 'semibold',
+    lineHeight: 24,
+  }
 });
